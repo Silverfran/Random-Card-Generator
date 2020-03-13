6800504444de0a3b4845b3d0d556025da016a180
SilverFran <Francisco1627@gmail.com>
2020-03-13 02:41:27 +0000

    random card generator

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,34 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let suit = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let value = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  let randomValue = value[Math.floor(Math.random() * value.length)];
+
+  if (randomSuit == "&hearts;" || randomSuit == "&diams;") {
+    document.querySelector("body").style.color = "red";
+  }
+
+  let card = `<div id="card">
+                <p id="content" data-content="${randomSuit}">${randomValue}</p>
+              </div>
+              <button type="button" class="btn btn-outline-primary" onclick="location.reload()" data-toggle="tooltip" data-placement="bottom" title="Hooray!"">Refresh</button>`;
+
+  document.querySelector("#container").innerHTML = card;
 };
